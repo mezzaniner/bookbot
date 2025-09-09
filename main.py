6269be0a1948803620dd2892fc40de1b8bc3e348
Mezz <mezzanine510@gmail.com>
2025-09-09 02:52:05 -0700

    finished up the mini-app!

---
+++ main.py
@@ -0,0 +1,29 @@
+from stats import get_num_words, get_letter_occurrences, get_sorted_list
+import sys
+
+def main():
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+    text = get_book_text(book_path)
+    word_count = get_num_words(text)
+    letter_count = get_letter_occurrences(text)
+    sorted_list = get_sorted_list(letter_count)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in sorted_list:
+        if item["char"].isalpha():
+            print(f'{item["char"]}: {item["num"]}')
+    print("============= END ===============")
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+main()
---
+++ stats.py
@@ -0,0 +1,28 @@
+def get_num_words(text):
+    words = len(text.split())
+    return words
+
+def get_letter_occurrences(text):
+    letters_dict = {}
+
+    for letter in text:
+        new_letter = letter.lower()
+        if new_letter in letters_dict:
+            letters_dict[new_letter] += 1
+        else:
+            letters_dict[new_letter] = 1
+
+    return letters_dict
+
+def get_sorted_list(dictionary):
+    dictionary_list = []
+
+    for key, value in dictionary.items():
+        pair = {"char": key, "num": value}
+        dictionary_list.append(pair)
+
+    def sort_on(list_to_sort):
+        return list_to_sort["num"]
+
+    dictionary_list.sort(reverse=True, key=sort_on)
+    return dictionary_list
